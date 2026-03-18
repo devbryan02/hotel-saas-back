f7b72026b2d754a1b2f2f92fa104426fe31887d1
bryancmy <brayan7br7@gmail.com>
2026-03-17 19:01:09 -0500

    refac url request in cors

--- src/main/java/com/app/hotelsaas/catin/infrastructure/security/SecurityConfig.java
+++ src/main/java/com/app/hotelsaas/catin/infrastructure/security/SecurityConfig.java
@@ -90,7 +90,7 @@ public class SecurityConfig {
         CorsConfiguration config = new CorsConfiguration();
 
         config.setAllowCredentials(true);
-        config.setAllowedOrigins(List.of("http://localhost:3000"));
+        config.setAllowedOrigins(List.of("https://zowy-app.vercel.app/"));
         config.setAllowedMethods(
             List.of("GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS")
         );
