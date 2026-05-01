cd92f51ad08b46b251ea38bdb869dfdf93e36750
bryancmy <brayan7br7@gmail.com>
2026-05-01 17:04:33 -0500

    refactor: formating code with alt + l XD

--- src/main/java/com/app/hotelsaas/catin/application/usecase/client/GetClientUseCase.java
+++ src/main/java/com/app/hotelsaas/catin/application/usecase/client/GetClientUseCase.java
@@ -18,21 +18,21 @@ public class GetClientUseCase {
     private final ClientRepository clientRepository;
     private final EntityFinder entityFinder;
 
-    public List<Client> findAllByTenantId(UUID tenantId){
+    public List<Client> findAllByTenantId(UUID tenantId) {
         return clientRepository.findAllByTenantId(tenantId);
     }
 
-    public Client findTenantIdAnId(UUID tenantId, UUID clientId){
+    public Client findTenantIdAnId(UUID tenantId, UUID clientId) {
         return entityFinder.findClient(tenantId, clientId);
     }
 
-    public List<Client> searchByTenantIdAndQuery(UUID tenantId, String query){
+    public List<Client> searchByTenantIdAndQuery(UUID tenantId, String query) {
         List<Client> clients = clientRepository.searchByTenantIdAndQuery(tenantId, query);
         log.info("Clients found: {} query: {}", clients, query);
         return clients;
     }
 
-    public List<Client> searchByTenantIdAndQueryAndStatus(UUID tenantId, String query, String status){
+    public List<Client> searchByTenantIdAndQueryAndStatus(UUID tenantId, String query, String status) {
         List<Client> clients = clientRepository.searchByTenantIdAndQueryAndStatus(tenantId, query, status);
         log.info("Clients found: {} query: {} status: {}", clients, query, status);
         return clients;
